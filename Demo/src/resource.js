27fb4457b6767d8ec2c08980f40b82bdfea0c786
Inconsolable523 <1273258990@qq.com>
2017-03-20 01:08:22 +0800

    refactor($browser):

--- Demo/src/resource.js
+++ Demo/src/resource.js
@@ -6,9 +6,9 @@ var res = {
     Bird2_png:"res/images/bird2.png",
     Bird3_png:"res/images/bird3.png",
     Bird4_png:"res/images/bird4.png",
-    Bird5_png:"res/images/bird5.png",
-    Bird6_png:"res/images/bird6.png",
-    Bird7_png:"res/images/bird7.png",
+    // Bird5_png:"res/images/bird5.png",
+    // Bird6_png:"res/images/bird6.png",
+    // Bird7_png:"res/images/bird7.png",
 
 };
 
